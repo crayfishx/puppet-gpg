e89b31f444bdde2360085343104372c889f30036
Uwe Stuehler <uwe@bsdx.de>
2013-09-20 12:33:49 +0200

    Plug a GPGME::Ctx leak with gpgme gem >= 2.0.0
    The latest version of gpgme on rubyforge is 2.0.2 and it supports
    passing a block to GPGME::Ctx.new.  I believe that up to now, this
    provider has leaked Ctx instances because Ctx#release was never
    called.  That's now ensured when Ctx.new is given a block.

--- lib/puppet/provider/gpgkey/gpgme.rb
+++ lib/puppet/provider/gpgkey/gpgme.rb
@@ -1,28 +1,31 @@
 Puppet::Type.type(:gpgkey).provide(:gpgme) do
+  # This provider uses the new API of the gpgme-2.0.0 gem.
   require 'gpgme'
+
   def exists?
     ! GPGME::Key.find(:secret, keyname()).empty?
   end
 
   def create
-    ctx = GPGME::Ctx.new
-    keydata = "<GnupgKeyParms format=\"internal\">\n"
-    keydata += "Key-Type: "       +@resource.value(:keytype)+"\n"
-    keydata += "Key-Length: "     +@resource.value(:keylength)+"\n"
-    keydata += "Subkey-Type: "    +@resource.value(:subkeytype)+"\n"
-    keydata += "Subkey-Length: "  +@resource.value(:subkeylength)+"\n"
-    keydata += "Name-Real: "      +@resource.value(:name)+"\n"
-    keydata += "Name-Comment: "   +keyname()+"\n"
-    keydata += "Name-Email: "     +@resource.value(:email)+"\n"
-    keydata += "Expire-Date: "    +@resource.value(:expire)+"\n"
-    # This parameter requires a value when present.  Default is to
-    # not use a passphrase.
-    unless @resource.value(:password).empty?
-      keydata += "Passphrase: "     +@resource.value(:password)+"\n"
-    end
-    keydata += "</GnupgKeyParms>\n"
+    GPGME::Ctx.new do |ctx|
+      keydata = "<GnupgKeyParms format=\"internal\">\n"
+      keydata += "Key-Type: "       +@resource.value(:keytype)+"\n"
+      keydata += "Key-Length: "     +@resource.value(:keylength)+"\n"
+      keydata += "Subkey-Type: "    +@resource.value(:subkeytype)+"\n"
+      keydata += "Subkey-Length: "  +@resource.value(:subkeylength)+"\n"
+      keydata += "Name-Real: "      +@resource.value(:name)+"\n"
+      keydata += "Name-Comment: "   +keyname()+"\n"
+      keydata += "Name-Email: "     +@resource.value(:email)+"\n"
+      keydata += "Expire-Date: "    +@resource.value(:expire)+"\n"
+      # This parameter requires a value when present.  Default is to
+      # not use a passphrase.
+      unless @resource.value(:password).empty?
+        keydata += "Passphrase: "     +@resource.value(:password)+"\n"
+      end
+      keydata += "</GnupgKeyParms>\n"
 
-    ctx.genkey(keydata, nil, nil)
+      ctx.genkey(keydata, nil, nil)
+    end
   end
 
   def destroy
